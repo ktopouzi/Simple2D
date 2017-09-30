c74d31eca10917c824fc5c7c79e16bfa1e7e420e
ktopouzi <kimon.topouzidis@gmail.com>
2017-09-30 20:44:56 +0300

    Added scrolling platforms and a way to die.

---
+++ 2D Platformer/Assets/Scripts/MovingElement.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingElement : MonoBehaviour
+{
+    public float backgroundSize;
+    public float paralaxSpeed;
+    public bool randomHeight;
+
+    private Transform cameraTransform;
+    public Transform[] layers;
+    private float viewZone = 10;
+    private int leftIndex;
+    private int rightIndex;
+    private float lastCameraX;
+
+    // Use this for initialization
+    void Start()
+    {
+        layers = new Transform[transform.childCount];
+        cameraTransform = Camera.main.transform;
+        lastCameraX = cameraTransform.position.x;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            layers[i] = transform.GetChild(i);
+        }
+
+        leftIndex = 0;
+        rightIndex = layers.Length - 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
+                ScrollLeft();
+
+            if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+                ScrollRight();
+        }
+        catch (Exception e)
+        {
+
+            print(e.Data);
+        }
+
+    }
+
+    private void ScrollLeft()
+    {
+        float currentY = transform.GetChild(0).position.y;
+        float s = UnityEngine.Random.Range(0, 3);
+        print(currentY);
+        int lastRight = rightIndex;
+        if (randomHeight)
+        {
+            layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize) + new Vector3(0, UnityEngine.Random.Range(currentY, 2), 0);
+        }else
+        {
+            layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize) + new Vector3(0, currentY, 0);
+        }  
+        leftIndex = rightIndex;
+        rightIndex--;
+        if (rightIndex < 0)
+            rightIndex = layers.Length - 1;
+
+    }
+
+    private void ScrollRight()
+    {
+        float currentY = transform.GetChild(0).position.y;
+        print(currentY);
+        int lastLeft = leftIndex;
+        if (randomHeight)
+        {
+            layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize) + new Vector3(0, UnityEngine.Random.Range(currentY, 2), 0);
+        }
+        else
+        {
+            layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize) + new Vector3(0, currentY, 0);
+        }
+       
+        rightIndex = leftIndex;
+        leftIndex++;
+        if (leftIndex == layers.Length)
+            leftIndex = 0;
+    }
+}
--- 2D Platformer/Assets/Scripts/Player.cs
+++ 2D Platformer/Assets/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
     public float timeToJumpApex = .4f;
     float accelerationTimeAirborne = .2f;
     float accelerationTimeGrounded = .1f;
-    float moveSpeed = 6;
+    public float moveSpeed = 6;
 
     public Vector2 wallJumpClimb;
     public Vector2 wallJumpOff;
